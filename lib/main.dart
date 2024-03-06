7a0883fdfe96de194b0bd3b0f66016742b4d1549
박상훈 <bbarkthong@gmail.com>
2024-03-06 17:10:46 +0900

    add and import text sections

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_drive/sections/ButtonSection.dart';
+import 'package:test_drive/sections/TextSection.dart';
 import 'package:test_drive/sections/TitleSection.dart';
 
 void main() => runApp(const MyApp());
@@ -23,7 +24,17 @@ class MyApp extends StatelessWidget {
                 name: 'Oeschinen Lake Campground', 
                 location: 'Kandersteg, Switzerland',
               ),
-              ButtonSection()
+              ButtonSection(),
+              TextSection(
+                description: 
+                  'Lake Oeschinen lies at the foot of the Blüemlisalp in the '            
+                  'Bernese Alps. Situated 1,578 meters above sea level, it '            
+                  'is one of the larger Alpine Lakes. A gondola ride from '            
+                  'Kandersteg, followed by a half-hour walk through pastures '            
+                  'and pine forest, leads you to the lake, which warms to 20 '            
+                  'degrees Celsius in the summer. Activities enjoyed here '            
+                  'include rowing, and riding the summer toboggan run.',            
+              )
             ],
           )
         ),
